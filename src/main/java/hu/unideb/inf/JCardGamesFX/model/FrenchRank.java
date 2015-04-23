b1ef0440b359d5042d4744a661a6187dc7d3a0c1
Zoltan Dalmadi <dmz985@gmail.com>
2015-04-23 20:12:52 +0200

    Added French Card Suits and Ranks

---
+++ src/main/java/hu/unideb/inf/JCardGamesFX/model/FrenchRank.java
@@ -0,0 +1,20 @@
+package hu.unideb.inf.JCardGamesFX.model;
+
+/**
+ * Standard French playing card ranks.
+ */
+public enum FrenchRank {
+  Ace,
+  Two,
+  Three,
+  Four,
+  Five,
+  Six,
+  Seven,
+  Eight,
+  Nine,
+  Ten,
+  Jack,
+  Queen,
+  King
+}
---
+++ src/main/java/hu/unideb/inf/JCardGamesFX/model/FrenchSuit.java
@@ -0,0 +1,11 @@
+package hu.unideb.inf.JCardGamesFX.model;
+
+/**
+ * Standard French playing card suits.
+ */
+public enum FrenchSuit {
+  Clubs,
+  Spades,
+  Hearts,
+  Diamonds
+}
