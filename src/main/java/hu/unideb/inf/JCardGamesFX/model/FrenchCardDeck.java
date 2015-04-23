afa7c47705d8658e7224d56e241cc733209f27c5
Zoltan Dalmadi <dmz985@gmail.com>
2015-04-23 20:15:13 +0200

    Added FrenchCardDeck class

--- src/main/java/hu/unideb/inf/JCardGamesFX/model/FrenchCardDeck.java
+++ src/main/java/hu/unideb/inf/JCardGamesFX/model/FrenchCardDeck.java
@@ -1,4 +1,22 @@
 package hu.unideb.inf.JCardGamesFX.model;
 
 public class FrenchCardDeck extends CardDeck {
+
+  /**
+   * Creates a standard 52-card deck of Frenck playing cards.
+   *
+   * @return The FrenchCardDeck object containing the 52 cards.
+   */
+  public static FrenchCardDeck createFrenchCardDeck() {
+
+    FrenchCardDeck result = new FrenchCardDeck();
+
+    for (FrenchSuit suit : FrenchSuit.values()) {
+      for (FrenchRank rank : FrenchRank.values()) {
+        result.addCard(new FrenchCard(true, suit, rank));
+      }
+    }
+
+    return result;
+  }
 }
