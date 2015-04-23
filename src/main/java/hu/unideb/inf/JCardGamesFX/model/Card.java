9c9dd3a84523dbd543da4a001c04d9a34b3fd87f
Zoltan Dalmadi <dmz985@gmail.com>
2015-04-23 20:13:37 +0200

    Added abstract Card class

--- src/main/java/hu/unideb/inf/JCardGamesFX/model/Card.java
+++ src/main/java/hu/unideb/inf/JCardGamesFX/model/Card.java
@@ -1,4 +1,48 @@
 package hu.unideb.inf.JCardGamesFX.model;
 
 public abstract class Card {
+
+  /**
+   * Whether the card is facing down.
+   */
+  protected boolean faceDown;
+
+  /**
+   * Constructs a Card object.
+   *
+   * @param faceDown Whether the card is facing down.
+   */
+  public Card(boolean faceDown) {
+    this.faceDown = faceDown;
+  }
+
+  /**
+   * Flips the card.
+   */
+  protected void flip() {
+    faceDown = !faceDown;
+  }
+
+  /**
+   * Turns the card down.
+   */
+  protected void faceDown() {
+    faceDown = true;
+  }
+
+  /**
+   * Turns the card up.
+   */
+  protected void faceUp() {
+    faceDown = false;
+  }
+
+  /**
+   * Returns whether the card is facing down.
+   *
+   * @return true if the card is facing down, false otherwise.
+   */
+  protected boolean isFaceDown() {
+    return faceDown;
+  }
 }
