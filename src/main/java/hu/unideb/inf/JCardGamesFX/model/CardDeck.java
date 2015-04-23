991883fbd3f76169882679f114eebe992bbed70b
Zoltan Dalmadi <dmz985@gmail.com>
2015-04-23 20:14:06 +0200

    Added abstract CardDeck class

--- src/main/java/hu/unideb/inf/JCardGamesFX/model/CardDeck.java
+++ src/main/java/hu/unideb/inf/JCardGamesFX/model/CardDeck.java
@@ -1,4 +1,56 @@
 package hu.unideb.inf.JCardGamesFX.model;
 
+import java.util.Collections;
+import java.util.List;
+
 public abstract class CardDeck {
+  /**
+   * A List holding the individual cards of this deck.
+   */
+  protected List<Card> cards;
+
+  /**
+   * Creates an empty CardDeck object.
+   */
+  public CardDeck() {
+  }
+
+  /**
+   * Returns the list of cards od this deck.
+   *
+   * @return The list of cards
+   */
+  public List<Card> getCards() {
+    return cards;
+  }
+
+  /**
+   * Adds a Card object to the deck.
+   *
+   * @param card The card to be added to the deck.
+   */
+  public void addCard(Card card) {
+    cards.add(card);
+  }
+
+  /**
+   * Removes a card from the deck.
+   *
+   * @param card The card to be removed.
+   * @return true if the card is removed from the deck, false otherwise.
+   */
+  public boolean removeCard(Card card) {
+    return cards.remove(card);
+  }
+
+  /**
+   * Shuffles the card deck.
+   * Researches proved that if we want a deck of cards to be well shuffled,
+   * we have to do it about seven times.
+   */
+  public void shuffle() {
+    for (int i = 0; i < 7; i++) {
+      Collections.shuffle(cards);
+    }
+  }
 }
