7a9d6a502338b383550e6b0eb3e270808b4bf58a
Zoltan Dalmadi <dmz985@gmail.com>
2015-04-24 22:55:52 +0200

    Added implementation and JavaDoc

--- src/main/java/hu/unideb/inf/JCardGamesFX/view/CardView.java
+++ src/main/java/hu/unideb/inf/JCardGamesFX/view/CardView.java
@@ -1,4 +1,120 @@
 package hu.unideb.inf.JCardGamesFX.view;
 
+import hu.unideb.inf.JCardGamesFX.model.Card;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ * This abstract class provides a base for classes that implements various
+ * JavaFX based views for {@link Card} objects.
+ *
+ * @author Zoltan Dalmadi
+ */
 public class CardView {
+  /**
+   * This static field holds the Image object that will be
+   * the back face of all cards.
+   */
+  protected static Image backFace;
+  /**
+   * The {@link Card} object to be represented.
+   */
+  protected Card card;
+  /**
+   * This field holds the Image object that will be the front face
+   * of an individual card.
+   */
+  protected Image frontFace;
+  /**
+   * This field is responsible for actually displaying
+   * the {@link Image} objects.
+   */
+  protected ImageView display;
+
+  /**
+   * Constructs a {@link CardView} object for the given {@link Card} object
+   * with the {@link Image} object as a representation in the game.
+   *
+   * @param card      The {@link Card} object to be represented.
+   * @param frontFace The {@link Image} object for representing the card.
+   */
+  public CardView(Card card, Image frontFace) {
+    this.card = card;
+    this.frontFace = frontFace;
+  }
+
+  /**
+   * Constructs a {@link CardView} object for the given {@link Card} object.
+   *
+   * @param card The {@link Card} object to be represented.
+   */
+  public CardView(Card card) {
+    this.card = card;
+  }
+
+  /**
+   * Constructs an empty {@link CardView}.
+   */
+  public CardView() {
+  }
+
+  /**
+   * Returns the {@link Image} object that holds the back face of all cards.
+   *
+   * @return The back face {@link Image} object.
+   */
+  public static Image getBackFace() {
+    return backFace;
+  }
+
+  /**
+   * Sets the {@link Image} object that holds the back face of all cards.
+   */
+  public static void setBackFace(Image backFace) {
+    CardView.backFace = backFace;
+  }
+
+  /**
+   * Returns the {@link Card} object that this object represents.
+   *
+   * @return The {@link Card} object.
+   */
+  public Card getCard() {
+    return card;
+  }
+
+  /**
+   * Sets the {@link Card} object that this object represents.
+   *
+   * @param card The {@link Card} object to be represented.
+   */
+  public void setCard(Card card) {
+    this.card = card;
+  }
+
+  /**
+   * Returns the {@link ImageView} object that displays the card face images.
+   *
+   * @return The {@link ImageView} object.
+   */
+  public ImageView getDisplay() {
+    return display;
+  }
+
+  /**
+   * Returns the {@link Image} object that holds the front face of this card.
+   *
+   * @return The back face {@link Image} object.
+   */
+  public Image getFrontFace() {
+
+    return frontFace;
+  }
+
+  /**
+   * Sets the {@link Image} object that holds the front face of this card.
+   */
+  public void setFrontFace(Image frontFace) {
+    this.frontFace = frontFace;
+  }
 }
--- src/main/java/hu/unideb/inf/JCardGamesFX/view/FrenchCardView.java
+++ src/main/java/hu/unideb/inf/JCardGamesFX/view/FrenchCardView.java
@@ -1,4 +1,39 @@
 package hu.unideb.inf.JCardGamesFX.view;
 
-public class FrenchCardView {
+import hu.unideb.inf.JCardGamesFX.model.FrenchCard;
+import javafx.scene.image.Image;
+
+/**
+ * This class provides a JavaFX based view for a {@link FrenchCard}.
+ *
+ * @author Zoltan Dalmadi
+ */
+public class FrenchCardView extends CardView {
+  /**
+   * Constructs a {@link FrenchCardView} object for the given
+   * {@link FrenchCard} object with the {@link Image} object as a
+   * representation in the game.
+   *
+   * @param card      The {@link FrenchCard} object to be represented.
+   * @param frontFace The {@link Image} object for representing the card.
+   */
+  public FrenchCardView(FrenchCard card, Image frontFace) {
+    super(card, frontFace);
+  }
+
+  /**
+   * Constructs a {@link FrenchCardView} object for the given
+   * {@link FrenchCard} object.
+   *
+   * @param card The {@link FrenchCard} object to be represented.
+   */
+  public FrenchCardView(FrenchCard card) {
+    super(card);
+  }
+
+  /**
+   * Constructs an empty FrenchCardView object.
+   */
+  public FrenchCardView() {
+  }
 }
