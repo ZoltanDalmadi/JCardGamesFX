17c79cc1942f62a1496f9a8550e6dd1d76bab422
Zoltan Dalmadi <dmz985@gmail.com>
2015-04-25 13:26:19 +0200

    Moved common code to superclass, utilize Observer pattern

--- src/main/java/hu/unideb/inf/JCardGamesFX/model/Card.java
+++ src/main/java/hu/unideb/inf/JCardGamesFX/model/Card.java
@@ -1,5 +1,10 @@
 package hu.unideb.inf.JCardGamesFX.model;
 
+import hu.unideb.inf.JCardGamesFX.view.CardObserver;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Abstract class representing the concept of a playing card.
  */
@@ -10,8 +15,22 @@ public abstract class Card {
    */
   protected boolean faceDown;
 
+  protected Suit suit;
+  protected Rank rank;
+
   /**
-   * Constructs a <code>Card</code> object.
+   * The list of views attached to this card.
+   */
+  List<CardObserver> views = new ArrayList<>();
+
+  public Card(boolean faceDown, Suit suit, Rank rank) {
+    this.faceDown = faceDown;
+    this.suit = suit;
+    this.rank = rank;
+  }
+
+  /**
+   * Constructs a {@link Card} object.
    *
    * @param faceDown Whether the card is facing down.
    */
@@ -20,24 +39,55 @@ public abstract class Card {
   }
 
   /**
+   * Returns the suit of this card.
+   *
+   * @return The Suit of this card.
+   */
+  public Suit getSuit() {
+    return suit;
+  }
+
+  /**
+   * Returns the rank of this card.
+   *
+   * @return The rank of this card.
+   */
+  public Rank getRank() {
+    return rank;
+  }
+
+  /**
+   * Returns a {@link String} representation of this card.
+   *
+   * @return The {@link String} representation of this card.
+   */
+  @Override
+  public String toString() {
+    return "The " + rank + " of " + suit;
+  }
+
+  /**
    * Flips the card.
    */
-  protected void flip() {
+  public void flip() {
     faceDown = !faceDown;
+    updateViews();
   }
 
   /**
    * Turns the card down.
    */
-  protected void faceDown() {
+  public void faceDown() {
     faceDown = true;
+    updateViews();
   }
 
   /**
    * Turns the card up.
    */
-  protected void faceUp() {
+  public void faceUp() {
     faceDown = false;
+    updateViews();
   }
 
   /**
@@ -45,7 +95,44 @@ public abstract class Card {
    *
    * @return true if the card is facing down, false otherwise.
    */
-  protected boolean isFaceDown() {
+  public boolean isFaceDown() {
     return faceDown;
   }
+
+  /**
+   * Adds a {@link CardObserver} to the list of views.
+   *
+   * @param view The {@link CardObserver} to be added.
+   */
+  public void addView(CardObserver view) {
+    views.add(view);
+    updateViews();
+  }
+
+  /**
+   * Removes a {@link CardObserver} from the list of views.
+   *
+   * @param view The {@link CardObserver} to be removed.
+   * @return true if successful, false otherwise.
+   */
+  public boolean removeView(CardObserver view) {
+    return views.remove(view);
+  }
+
+  /**
+   * Returns the list of views attached to this card.
+   *
+   * @return The list of views.
+   */
+  public List<CardObserver> getViews() {
+    return views;
+  }
+
+  /**
+   * Updates all views attached to this card.
+   */
+  protected void updateViews() {
+    views.forEach(CardObserver::update);
+  }
+
 }
--- src/main/java/hu/unideb/inf/JCardGamesFX/model/FrenchCard.java
+++ src/main/java/hu/unideb/inf/JCardGamesFX/model/FrenchCard.java
@@ -6,16 +6,6 @@ package hu.unideb.inf.JCardGamesFX.model;
 public class FrenchCard extends Card {
 
   /**
-   * Suit of this card object.
-   */
-  private FrenchSuit suit;
-
-  /**
-   * Rank of this card object.
-   */
-  private FrenchRank rank;
-
-  /**
    * Constructs a {@link FrenchCard} object.
    *
    * @param faceDown Whether the card is facing down.
@@ -23,36 +13,7 @@ public class FrenchCard extends Card {
    * @param rank     Rank of the card.
    */
   public FrenchCard(boolean faceDown, FrenchSuit suit, FrenchRank rank) {
-    super(faceDown);
-    this.suit = suit;
-    this.rank = rank;
+    super(faceDown, suit, rank);
   }
 
-  /**
-   * Returns the suit of this card.
-   *
-   * @return The Suit of this card.
-   */
-  public FrenchSuit getSuit() {
-    return suit;
-  }
-
-  /**
-   * Returns the rank of this card.
-   *
-   * @return The rank of this card.
-   */
-  public FrenchRank getRank() {
-    return rank;
-  }
-
-  /**
-   * Returns a {@link String} representation of this card.
-   *
-   * @return The {@link String} representation of this card.
-   */
-  @Override
-  public String toString() {
-    return "The " + rank + " of " + suit;
-  }
 }
