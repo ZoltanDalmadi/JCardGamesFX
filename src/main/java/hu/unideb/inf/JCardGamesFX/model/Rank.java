3d41093f69f734435be2d7965092d15758767b2e
Zoltan Dalmadi <dmz985@gmail.com>
2015-04-25 14:29:57 +0200

    Added JavaDoc description

--- src/main/java/hu/unideb/inf/JCardGamesFX/model/Rank.java
+++ src/main/java/hu/unideb/inf/JCardGamesFX/model/Rank.java
@@ -1,4 +1,7 @@
 package hu.unideb.inf.JCardGamesFX.model;
 
+/**
+ * Common interface for playing card ranks.
+ */
 public interface Rank {
 }
--- src/main/java/hu/unideb/inf/JCardGamesFX/model/Suit.java
+++ src/main/java/hu/unideb/inf/JCardGamesFX/model/Suit.java
@@ -1,4 +1,7 @@
 package hu.unideb.inf.JCardGamesFX.model;
 
+/**
+ * Common interface for playing card suits.
+ */
 public interface Suit {
 }
