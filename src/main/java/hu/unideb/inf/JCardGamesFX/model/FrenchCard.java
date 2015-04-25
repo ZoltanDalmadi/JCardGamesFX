2f05ed7956b3c201f545bf5b4959174aedf4b2db
Zoltan Dalmadi <dmz985@gmail.com>
2015-04-25 16:35:33 +0200

    Added implementation for getId() method

--- src/main/java/hu/unideb/inf/JCardGamesFX/model/FrenchCard.java
+++ src/main/java/hu/unideb/inf/JCardGamesFX/model/FrenchCard.java
@@ -25,4 +25,83 @@ public class FrenchCard extends Card {
     super(faceDown, suit, rank);
   }
 
+  /**
+   * Returns a short {@link String} identifier.
+   *
+   * @return The short identifier as a {@link String}.
+   */
+  @Override
+  public String getId() {
+
+    String suitChar;
+    String rankChar;
+
+    FrenchRank rank = (FrenchRank) this.getRank();
+
+    switch (rank) {
+      case Ace:
+        rankChar = "A";
+        break;
+      case Two:
+        rankChar = "2";
+        break;
+      case Three:
+        rankChar = "3";
+        break;
+      case Four:
+        rankChar = "4";
+        break;
+      case Five:
+        rankChar = "5";
+        break;
+      case Six:
+        rankChar = "6";
+        break;
+      case Seven:
+        rankChar = "7";
+        break;
+      case Eight:
+        rankChar = "8";
+        break;
+      case Nine:
+        rankChar = "9";
+        break;
+      case Ten:
+        rankChar = "10";
+        break;
+      case Jack:
+        rankChar = "J";
+        break;
+      case Queen:
+        rankChar = "Q";
+        break;
+      case King:
+        rankChar = "K";
+        break;
+      default:
+        rankChar = "";
+    }
+
+    FrenchSuit suit = (FrenchSuit) this.getSuit();
+
+    switch (suit) {
+      case Clubs:
+        suitChar = "C";
+        break;
+      case Diamonds:
+        suitChar = "D";
+        break;
+      case Hearts:
+        suitChar = "H";
+        break;
+      case Spades:
+        suitChar = "S";
+        break;
+      default:
+        suitChar = "";
+    }
+
+    return rankChar + suitChar;
+  }
+
 }
