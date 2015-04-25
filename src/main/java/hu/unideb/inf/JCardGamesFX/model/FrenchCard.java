c856e0d410c70749a418f98c9f7c49888c9dc921
Zoltan Dalmadi <dmz985@gmail.com>
2015-04-25 14:24:28 +0200

    Added another constructor

--- src/main/java/hu/unideb/inf/JCardGamesFX/model/FrenchCard.java
+++ src/main/java/hu/unideb/inf/JCardGamesFX/model/FrenchCard.java
@@ -9,6 +9,15 @@ public class FrenchCard extends Card {
    * Constructs a {@link FrenchCard} object.
    *
    * @param faceDown Whether the card is facing down.
+   */
+  public FrenchCard(boolean faceDown) {
+    super(faceDown);
+  }
+
+  /**
+   * Constructs a {@link FrenchCard} object, with the specified suit and rank.
+   *
+   * @param faceDown Whether the card is facing down.
    * @param suit     Suit of the card.
    * @param rank     Rank of the card.
    */
