f86462cdf4f5fb280a23241ba53d8831e295a54a
Zoltan Dalmadi <dmz985@gmail.com>
2015-04-26 12:28:38 +0200

    Rewritten test case for CardTheme class

--- src/test/java/hu/unideb/inf/JCardGamesFX/view/CardThemeTest.java
+++ src/test/java/hu/unideb/inf/JCardGamesFX/view/CardThemeTest.java
@@ -1,52 +1,69 @@
 package hu.unideb.inf.JCardGamesFX.view;
 
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class CardThemeTest {
 
   CardTheme theme;
 
+  @BeforeClass
+  public static void initJFX() throws Exception {
+    Thread t = new Thread("JavaFX init thread") {
+      public void run() {
+        Application.launch(TestApp.class);
+      }
+    };
+    t.setDaemon(true);
+    t.start();
+    Thread.sleep(1000);
+  }
+
   @Before
   public void setUp() {
-    theme = new CardTheme("/cardfaces/classicTest/theme.json", "/test_back.png");
+    theme = new CardTheme();
   }
 
   @Test
   public void testThemeFileOperations() {
-    theme.setThemeFile("TEST");
-    assertEquals("TEST", theme.getThemeFile());
-
-    CardTheme theme2 = new CardTheme();
-    theme2.setThemeFile("TEST");
-    assertEquals("TEST", theme2.getThemeFile());
+    theme.setThemeFile("/cardfaces/classicTest/theme.json");
+    assertEquals("/cardfaces/classicTest/theme.json", theme.getThemeFile());
   }
 
   @Test
   public void testBackFacePathOperations() {
-    theme.setBackFacePath("TEST");
-    assertEquals("TEST", theme.getBackFacePath());
-
-    CardTheme theme2 = new CardTheme("TEST");
-    assertEquals("TEST", theme2.getBackFacePath());
+    Image testBackFace = new Image("/test_back.png");
+    theme.setBackFace(testBackFace);
+    assertSame(testBackFace, theme.getBackFace());
   }
 
   @Test
   public void testMapOperations() {
-    theme.addFrontFacePathFor("TEST KEY", "TEST VALUE");
-    assertEquals("TEST VALUE", theme.getFrontFacePathFor("TEST KEY"));
-    theme.removeFrontFacePath("TEST KEY");
-    assertTrue(theme.getPaths().isEmpty());
+    Image testFrontFace = new Image("/cardfaces/classicTest/10C.png");
+    theme.addFrontFace("TEST KEY", testFrontFace);
+    assertSame(testFrontFace, theme.getFrontFace("TEST KEY"));
+    theme.removeFrontFace("TEST KEY");
+    assertTrue(theme.getImages().isEmpty());
   }
 
   @Test
-  public void testParseTheme() throws Exception {
-    theme.parseTheme();
-    assertEquals(13, theme.getPaths().size());
-    assertEquals("/cardfaces/classicTest/10C.png", theme.getFrontFacePathFor("10C"));
-    assertEquals("/cardfaces/classicTest/QC.png", theme.getFrontFacePathFor("QC"));
+  public void testParseTheme() {
+    theme.setBackFace(new Image("/test_back.png"));
+    theme.setThemeFile("/cardfaces/classicTest/theme.json");
+    assertEquals(13, theme.getImages().size());
+    assertEquals(130.0, theme.getFrontFace("10C").getWidth(), 0.0);
+    assertEquals(180.0, theme.getFrontFace("QC").getHeight(), 0.0);
+  }
+
+  public static class TestApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    }
   }
 }
