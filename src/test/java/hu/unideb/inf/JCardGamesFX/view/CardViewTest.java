567a7568ff5704d3aaeee32fc365d6286d2de821
Zoltan Dalmadi <dmz985@gmail.com>
2015-05-01 13:20:08 +0200

    Added  Unit test for CardView class

---
+++ src/test/java/hu/unideb/inf/JCardGamesFX/view/CardViewTest.java
@@ -0,0 +1,52 @@
+package hu.unideb.inf.JCardGamesFX.view;
+
+import org.json.simple.parser.ParseException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unit test for class {@link CardView}.
+ */
+public class CardViewTest {
+
+  /**
+   * Rule for testing in a JavaFX thread.
+   */
+  @Rule
+  public JavaFXThreadingRule jfxRule = new JavaFXThreadingRule();
+  CardTheme theme;
+  CardView cardView;
+
+  /**
+   * Creates a CardView object and a {@link CardTheme} object before testing.
+   */
+  @Before
+  public void setUp() {
+    cardView = new CardView();
+    try {
+      theme = new CardTheme("/cardfaces/classicTest/theme.json", "/test_back.png");
+    } catch (IOException | ParseException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Tests all methods.
+   */
+  @Test
+  public void testGettersAndSettersAndFlip() {
+    cardView.setBackFace(theme.getBackFace());
+    assertEquals(theme.getBackFace(), cardView.getBackFace());
+    cardView.setFrontFace(theme.getFrontFace("KC"));
+    assertEquals(theme.getFrontFace("KC"), cardView.getFrontFace());
+    assertEquals(theme.getFrontFace("KC"), cardView.getImage());
+    cardView.flip();
+    assertEquals(theme.getBackFace(), cardView.getImage());
+  }
+
+}
