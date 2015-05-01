7446aee4e6f17d67098f606a8d8f8c6948e19de2
Zoltan Dalmadi <dmz985@gmail.com>
2015-05-01 20:34:16 +0200

    Added background texture, dummy statusbar / menu

--- src/main/java/hu/unideb/inf/JCardGamesFX/klondike/KlondikeGame.java
+++ src/main/java/hu/unideb/inf/JCardGamesFX/klondike/KlondikeGame.java
@@ -1,13 +1,18 @@
 package hu.unideb.inf.JCardGamesFX.klondike;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class KlondikeGame extends Application {
@@ -21,17 +26,26 @@ public class KlondikeGame extends Application {
 
   @Override
   public void start(Stage primaryStage) {
-    // middle part
-    Pane root = new Pane();
-    root.setBackground(new Background(new BackgroundFill(Color.OLIVEDRAB, null, null)));
+    // Game area
+    Pane gameArea = new Pane();
 
-    // top bar
-    MenuBar menuBar = new MenuBar();
+    Image tableaouBackground = new Image("/tableaous/green-felt.png");
+
+    gameArea.setBackground(new Background(new BackgroundImage(tableaouBackground,
+        BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, null, null)));
+
+    // TODO: change to actual menubar class
+    MenuBar menuBar = new MenuBar(new Menu("Dummy Menu"));
+
+    // TODO: change to actual status bar class
+    HBox statusBar = new HBox(20, new Label("Dummy status bar text"));
+    statusBar.setPadding(new Insets(2));
 
     // main element
     BorderPane bord = new BorderPane();
-    bord.setCenter(root);
+    bord.setCenter(gameArea);
     bord.setTop(menuBar);
+    bord.setBottom(statusBar);
 
     // scene
     Scene scene = new Scene(bord, WIDTH, HEIGHT);
