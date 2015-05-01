82241984824c485f2564eeeec58070c33ed1e226
Zoltan Dalmadi <dmz985@gmail.com>
2015-05-01 12:50:10 +0200

    Big changes: CardView now extends Image, and doesn't know about the corresponding Card object

--- src/main/java/hu/unideb/inf/JCardGamesFX/view/CardView.java
+++ src/main/java/hu/unideb/inf/JCardGamesFX/view/CardView.java
@@ -8,7 +8,7 @@ import javafx.scene.image.ImageView;
  * This abstract class provides a base for classes that implements various
  * JavaFX based views for {@link Card} objects.
  */
-public class CardView implements CardObserver {
+public class CardView extends ImageView {
 
   /**
    * This field holds the Image object that will be
@@ -17,43 +17,23 @@ public class CardView implements CardObserver {
   private Image backFace;
 
   /**
-   * The {@link Card} object to be represented.
-   */
-  private Card card;
-
-  /**
    * This field holds the Image object that will be the front face
    * of this card.
    */
   private Image frontFace;
 
   /**
-   * This field is responsible for actually displaying
-   * the {@link Image} objects.
-   */
-  private ImageView display = new ImageView();
-
-  /**
-   * Constructs a {@link CardView} object for the given {@link Card} object
-   * with the {@link Image} object as a representation in the game.
+   * Constructs a {@link CardView} object with the two {@link Image} objects
+   * as a representation in the game.
    *
-   * @param card      The {@link Card} object to be represented.
-   * @param frontFace The {@link Image} object for representing the card.
+   * @param frontFace The {@link Image} object for the front face.
    * @param backFace  The {@link Image} object for the back face.
    */
-  public CardView(Card card, Image frontFace, Image backFace) {
-    this.card = card;
+  public CardView(Image frontFace, Image backFace) {
     this.frontFace = frontFace;
     this.backFace = backFace;
-  }
 
-  /**
-   * Constructs a {@link CardView} object for the given {@link Card} object.
-   *
-   * @param card The {@link Card} object to be represented.
-   */
-  public CardView(Card card) {
-    this.card = card;
+    setImage(frontFace);
   }
 
   /**
@@ -81,33 +61,6 @@ public class CardView implements CardObserver {
   }
 
   /**
-   * Returns the {@link Card} object that this object represents.
-   *
-   * @return The {@link Card} object.
-   */
-  public Card getCard() {
-    return card;
-  }
-
-  /**
-   * Sets the {@link Card} object that this object represents.
-   *
-   * @param card The {@link Card} object to be represented.
-   */
-  public void setCard(Card card) {
-    this.card = card;
-  }
-
-  /**
-   * Returns the {@link ImageView} object that displays the card face images.
-   *
-   * @return The {@link ImageView} object.
-   */
-  public ImageView getDisplay() {
-    return display;
-  }
-
-  /**
    * Returns the {@link Image} object that holds the front face of this card.
    *
    * @return The back face {@link Image} object.
@@ -126,11 +79,10 @@ public class CardView implements CardObserver {
   }
 
   /**
-   * Sets the current {@link Image} object for the <code>display</code> field.
+   * Swaps the displayed image of the card.
    */
-  @Override
-  public void update() {
-    display.setImage(card.isFaceDown() ? backFace : frontFace);
+  public void flip() {
+    setImage(getImage().equals(frontFace) ? backFace : frontFace);
   }
 
 }
